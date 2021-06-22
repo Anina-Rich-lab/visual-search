22a140488ed56115d4ee7a1661fd790c5d9a2f73
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-06-22 17:24:07 +1000

    Added code to randomize trial.

--- visual-search/experiment.py
+++ visual-search/experiment.py
@@ -1,8 +1,9 @@
 import argparse
 import os
+import csv
 from math import sin, cos, pi
 from random import random
-from typing import Dict, List
+from typing import Dict, List, Optional
 from psychopy import visual, core, event
 from psychopy.hardware import keyboard
 
@@ -11,34 +12,84 @@ INVALID_STIMULI_DIR = "false"
 
 
 class VisualSearch:
-    def __init__(self, stimuli_location: str) -> None:
+    def __init__(self, stimuli_location: str, circle_rad: float) -> None:
         self.window = visual.Window(fullscr=True, monitor="testMonitor", units="deg")
+        self.fixation = visual.ShapeStim(self.window,
+                                         vertices=((0, -0.5), (0, 0.5), (0, 0), (-0.5, 0), (0.5, 0)),
+                                         lineWidth=2,
+                                         closeShape=False,
+                                         lineColor="black")
         self.stimuli = self.load_stimuli(stimuli_location)
         self.kb = keyboard.Keyboard()
+        self.rad = circle_rad
 
-    def place_stimuli(self, r: float) -> None:
+    def get_valid(self) -> List[visual.ImageStim]:
+        return random.sample(self.stimuli["valid"], 1)
+
+    def get_invalid(self, n: int) -> List[visual.ImageStim]:
+        return random.sample(self.stimuli["valid"], n)
+
+    def place_stimuli(self, nc: int, is_target_present: bool) -> List[visual.ImageStim]:
         """ Place randomly the stimuli around a circle with radius `r`. """
-        stimuli = self.stimuli["valid"] + self.stimuli["invalid"]
+
+        stimuli = self.get_valid() + self.get_invalid(nc - 1) if is_target_present else self.get_invalid(nc)
+        random.shuffle(stimuli)
 
         angle_div = 2 * pi / len(stimuli)
         rand_offset = random() * 2 * pi
 
         for i, s in enumerate(stimuli):
             angle = angle_div * i + rand_offset
-            s.pos = [r * cos(angle), r * sin(angle)]
+            s.pos = [self.rad * cos(angle), self.rad * sin(angle)]
 
-    def draw(self) -> None:
-        stimuli = self.stimuli["valid"] + self.stimuli["invalid"]
+        return stimuli
+
+    def show_items(self, stimuli: List[visual.ImageStim], show_fix: Optional[bool] = True) -> None:
         for s in stimuli:
             s.draw()
+        if show_fix:
+            self.fixation.draw()
         self.window.update()
 
-    def run_trial(self, timeout=float('inf')) -> None:
+    def show_fixation(self) -> None:
+        self.fixation.draw()
+        self.window.update()
+
+    def show_feedback(self, success: bool) -> None:
+        if success:
+            self.success_visual.draw()
+        else:
+            self.failure_visual.draw()
+        self.window.update()
+
+    def run_trial(self,
+                  feedback_timeout: Optional[float] = 3.0,
+                  fixation_timeout: Optional[float] = 2.0,
+                  response_timeout: Optional[float] = float('inf')) -> None:
+        """
+            Each trial runs in three stages:
+            1. Show fixation screen for a fixed period `fixation_timeout`.
+            2. Show a number of items `n_items`, then wait for keyboard response up to a limit `response_timeout`.
+            3. Show feedback for a `feedback_timeout`.
+        """
+        # TODO: 0. Get items for experiment
+        stimuli = self.place_stimuli(nc=4, is_target_present=True)
+
+        # 1. Show blank with fixation.
+        self.show_fixation()
+        core.wait(fixation_timeout)
+
+        # 2. Redraw now with items, wait for response
+        self.show_items(stimuli)
         self.kb.clock.reset()
-        keys = self.kb.waitKeys(maxWait=timeout, keyList=['x', 'm'])
+        keys = self.kb.waitKeys(maxWait=response_timeout, keyList=['x', 'm'])
         for key in keys:
             # TODO: store results.
             print(key.name, key.rt, key.duration)
+
+        # 3. Show feedback.
+        self.show_feedback(True)
+        core.wait(feedback_timeout)
 
     def load_stimuli(self, location: str) -> Dict[str, List[visual.ImageStim]]:
         # Check if location exists.
@@ -64,28 +115,30 @@ class VisualSearch:
 
         return stimuli
 
+    def store_data(self, info: dict) -> None:
+        with open('path/to/csv_file', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow()
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-d", default="stimuli", help="Address of directory containin stimuli.", type=str)
+    parser.add_argument("-r", default=5.0, help="Distance of stimuli from center.", type=float)
 
     args = parser.parse_args()
 
-    vs = VisualSearch(args.d)
+    vs = VisualSearch(stimuli_location=args.d,
+                      circle_rad=args.r)
 
-    r = 10
-
-    def change_rad():
-        global r
-        r += 1
-        vs.place_stimuli(r)
-        vs.draw()
+    def start():
+        vs.rad += 1
         vs.run_trial()
 
     event.globalKeys.add(key='q', modifiers=['ctrl'], func=core.quit)
-    event.globalKeys.add(key='r', func=change_rad)
+    event.globalKeys.add(key='r', func=start)
 
     while True:
         core.wait(0.001)
