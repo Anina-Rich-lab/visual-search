618708c80981003b4f2b061424edd4181f647045
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-06-24 12:01:54 +1000

    Finished run generation.

--- visualsearch/experiment.py
+++ visualsearch/experiment.py
@@ -12,9 +12,21 @@ INVALID_STIMULI_DIR = "false"
 package_folder = os.path.dirname(os.path.abspath(__file__))
 
 
+def gen_trials(n_trials):
+    trials = [True if i < n_trials // 2 else False for i in range(n_trials)]
+    random.shuffle(trials)
+    return trials
+
+
 class VisualSearch:
-    def __init__(self, stimuli_location: str, circle_rad: float) -> None:
+    def __init__(self,
+                 stimuli_location: str,
+                 circle_rad: float,
+                 target_present_key: Optional[str] = 'x',
+                 target_not_present_key: Optional[str] = 'm') -> None:
+
         self.window = visual.Window(fullscr=True, monitor="testMonitor", units="deg")
+        event.globalKeys.add(key='q', modifiers=['ctrl'], func=core.quit)
         self.fixation = visual.ShapeStim(self.window,
                                          vertices=((0, -0.5), (0, 0.5), (0, 0), (-0.5, 0), (0.5, 0)),
                                          lineWidth=2,
@@ -22,15 +34,24 @@ class VisualSearch:
                                          lineColor="black")
         self.tick = visual.ImageStim(win=self.window, image=os.path.join(package_folder, "misc", "tick.png"), size=2)
         self.fail = visual.ImageStim(win=self.window, image=os.path.join(package_folder, "misc", "fail.png"), size=2)
+
         self.stimuli = self.load_stimuli(stimuli_location)
         self.kb = keyboard.Keyboard()
         self.rad = circle_rad
+        self.tp_key = target_present_key
+        self.ntp_key = target_not_present_key
+
+    def get_image_stim(self, sid: str, n: int, repeat: Optional[bool] = True) -> List[visual.ImageStim]:
+        if repeat:
+            return [visual.ImageStim(win=self.window, image=random.choice(self.stimuli[sid]), size=2) for _ in range(n)]
+        else:
+            return [visual.ImageStim(win=self.window, image=f, size=2) for f in random.sample(self.stimuli[sid], n)]
 
     def get_valid(self) -> List[visual.ImageStim]:
-        return random.sample(self.stimuli["valid"], 1)
+        return self.get_image_stim("valid", 1, repeat=False)
 
     def get_invalid(self, n: int) -> List[visual.ImageStim]:
-        return random.sample(self.stimuli["invalid"], n)
+        return self.get_image_stim("invalid", n)
 
     def place_stimuli(self, nc: int, is_target_present: bool) -> List[visual.ImageStim]:
         """ Place randomly the stimuli around a circle with radius `r`. """
@@ -66,6 +87,8 @@ class VisualSearch:
         self.window.update()
 
     def run_trial(self,
+                  is_target_present: bool,
+                  n_conditions: int,
                   feedback_timeout: Optional[float] = 3.0,
                   fixation_timeout: Optional[float] = 2.0,
                   response_timeout: Optional[float] = float('inf')) -> None:
@@ -75,8 +98,8 @@ class VisualSearch:
             2. Show a number of items `n_items`, then wait for keyboard response up to a limit `response_timeout`.
             3. Show feedback for a `feedback_timeout`.
         """
-        # TODO: 0. Get items for experiment
-        stimuli = self.place_stimuli(nc=2, is_target_present=True)
+        # 0. Get items for experiment
+        stimuli = self.place_stimuli(nc=n_conditions, is_target_present=is_target_present)
 
         # 1. Show blank with fixation.
         self.show_fixation()
@@ -85,13 +108,14 @@ class VisualSearch:
         # 2. Redraw now with items, wait for response
         self.show_items(stimuli)
         self.kb.clock.reset()
-        keys = self.kb.waitKeys(maxWait=response_timeout, keyList=['x', 'm'])
+        keys = self.kb.waitKeys(maxWait=response_timeout, keyList=[self.tp_key, self.ntp_key])
         for key in keys:
+            correct = key.name == (self.tp_key if is_target_present else self.ntp_key)
             # TODO: store results.
-            print(key.name, key.rt, key.duration)
+            print(key.name, key.rt, correct)
 
         # 3. Show feedback.
-        self.show_feedback(True)
+        self.show_feedback(correct)
         core.wait(feedback_timeout)
 
         # 4. Clear the screen.
@@ -114,12 +138,18 @@ class VisualSearch:
         # Now, we can get all images in the folders.
         stimuli = {"valid": [], "invalid": []}
         for im in os.listdir(valid_folder):
-            stimuli["valid"].append(visual.ImageStim(win=self.window, image=os.path.join(valid_folder, im), size=2))
+            stimuli["valid"].append(os.path.join(valid_folder, im))
 
         for im in os.listdir(invalid_folder):
-            stimuli["invalid"].append(visual.ImageStim(win=self.window, image=os.path.join(invalid_folder, im), size=2))
+            stimuli["invalid"].append(os.path.join(invalid_folder, im))
 
         return stimuli
+
+    def run_test(self, n_conditions: List[int], n_repetitions: List[int]) -> None:
+        for (n_cond, n_rep) in zip(n_conditions, n_repetitions):
+            trials = gen_trials(n_rep)
+            for t in trials:
+                self.run_trial(is_target_present=t, n_conditions=n_cond)
 
     def store_data(self, info: dict) -> None:
         with open('path/to/csv_file', 'w') as f:
@@ -139,12 +169,4 @@ if __name__ == '__main__':
     vs = VisualSearch(stimuli_location=args.d,
                       circle_rad=args.r)
 
-    def start():
-        vs.rad += 1
-        vs.run_trial()
-
-    event.globalKeys.add(key='q', modifiers=['ctrl'], func=core.quit)
-    event.globalKeys.add(key='r', func=start)
-
-    while True:
-        core.wait(0.001)
+    vs.run_test([8, 12, 16], [5, 3, 1])
